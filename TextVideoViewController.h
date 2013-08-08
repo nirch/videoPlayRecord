ca217fc76f4c0f5ed01035d04354a05a7f936a73
nirch <nirh2@yahoo.com>
2013-08-08 14:19:25 +0300

    Text Video: Creating a text video based on an image

--- TextVideoViewController.h
+++ TextVideoViewController.h
@@ -16,9 +16,14 @@
 @interface TextVideoViewController : UIViewController <UITextFieldDelegate>
 
 @property(nonatomic, strong) AVURLAsset *videoAsset;
+@property(nonatomic, strong) UIImage *backgroundImage;
 @property (weak, nonatomic) IBOutlet UITextField *inputText;
 
 - (IBAction)createTextVideo:(id)sender;
 -(void)exportDidFinish:(AVAssetExportSession*)session;
+- (IBAction)createVideoByImage:(id)sender;
+
+- (UIImage*) scaleImage: (UIImage*)originalImage toSize: (CGSize)newSize;
+- (CVPixelBufferRef) newPixelBufferFromCGImage: (CGImageRef) image;
 
 @end
--- TextVideoViewController.m
+++ TextVideoViewController.m
@@ -28,12 +28,18 @@
     [super viewDidLoad];
 	// Do any additional setup after loading the view.
     
+    // Loading the background image
+    NSString *backgroundfImagePath = [[NSBundle mainBundle] pathForResource:@"background" ofType:@"jpg"];
+    _backgroundImage = [UIImage imageWithContentsOfFile:backgroundfImagePath];
+
+    
     // Getting a path and a URL to the video in this project
     NSString *videoPath = [[NSBundle mainBundle] pathForResource:@"Red" ofType:@"mov"];
     NSURL   *videoURL = [NSURL fileURLWithPath:videoPath];
     
     // Initialaing the video asset with the video embedded in this project
     _videoAsset = [[AVURLAsset alloc]initWithURL:videoURL  options:nil];
+    
 }
 
 - (void)didReceiveMemoryWarning
@@ -147,6 +153,152 @@
     }
     NSLog(@"Completed");
 }
+
+// This method creates a video using an image and text
+- (IBAction)createVideoByImage:(id)sender {
+
+    NSError *error = nil;
+    
+    // Getting the path to the documents directory
+    NSArray *paths = NSSearchPathForDirectoriesInDomains(NSDocumentDirectory, NSUserDomainMask, YES);
+    NSString *documentsDirectory = [paths objectAtIndex:0];
+    
+    // Creating a full path and URL to the image video
+    NSString *myPathDocs =  [documentsDirectory stringByAppendingPathComponent:
+                             [NSString stringWithFormat:@"imageVideo-%d.mov",arc4random() % 1000]];
+    NSURL *outptUrl = [NSURL fileURLWithPath:myPathDocs];
+    
+    // Creating the container to which the video will be written to
+    AVAssetWriter *videoWriter = [[AVAssetWriter alloc] initWithURL:outptUrl fileType:AVFileTypeQuickTimeMovie
+                                                              error:&error];
+    NSParameterAssert(videoWriter);
+    
+    // Specifing settings for the new video (codec, width, hieght)
+    NSDictionary *videoSettings = [NSDictionary dictionaryWithObjectsAndKeys:
+                                   AVVideoCodecH264, AVVideoCodecKey,
+                                   [NSNumber numberWithInt:640], AVVideoWidthKey,
+                                   [NSNumber numberWithInt:480], AVVideoHeightKey,
+                                   nil];
+    
+    // Creating a writer input
+    AVAssetWriterInput* writerInput = [AVAssetWriterInput
+                                       assetWriterInputWithMediaType:AVMediaTypeVideo
+                                       outputSettings:videoSettings];
+    
+    NSParameterAssert(writerInput);
+    NSParameterAssert([videoWriter canAddInput:writerInput]);
+    
+    // Connecting the writer input with the video wrtier
+    [videoWriter addInput:writerInput];
+    
+    // Creating an AVAssetWriterInputPixelBufferAdaptor based on writerInput
+    AVAssetWriterInputPixelBufferAdaptor *assetWriterInputPixelAdapter = [AVAssetWriterInputPixelBufferAdaptor assetWriterInputPixelBufferAdaptorWithAssetWriterInput:writerInput sourcePixelBufferAttributes:nil];
+    
+    // Start writing
+    [videoWriter startWriting];
+    
+    // The duration of each frame in the video is "frameTime". The present time for each fram will start at 0 and then we will add the frame time to the present time for each frame
+    CMTime frameTime = CMTimeMake(3000, 1000);
+    CMTime presentTime = CMTimeMake(0, 1000);
+    [videoWriter startSessionAtSourceTime:kCMTimeZero];
+
+    // Resizing image to 640:480
+    UIImage *scaledBackgroundImage = [self scaleImage:_backgroundImage toSize:CGSizeMake(640.0, 480.0)];
+        
+    // Appending image to asset writer
+    BOOL appendSuccess = [assetWriterInputPixelAdapter appendPixelBuffer:[self newPixelBufferFromCGImage:scaledBackgroundImage.CGImage] withPresentationTime:presentTime];
+    NSLog(appendSuccess ? @"Append Success" : @"Append Failed");
+        
+    // Increasing the present time
+    presentTime = CMTimeAdd(presentTime,frameTime);
+
+    // There is a need to append it again in the last time (otherwise the video will always be 0 seconds long)
+    BOOL appendSuccess2 = [assetWriterInputPixelAdapter appendPixelBuffer:[self newPixelBufferFromCGImage:scaledBackgroundImage.CGImage] withPresentationTime:presentTime];
+    NSLog(appendSuccess2 ? @"Append Success" : @"Append Failed");
+    
+    
+    // Finishing the video. The actaul finish process is asynchronic, so we are assigning a completion handler to be invoked once the the video is ready ("videoWriterDidFinish")
+    [writerInput markAsFinished];
+    [videoWriter endSessionAtSourceTime:presentTime];
+    [videoWriter finishWritingWithCompletionHandler:^{
+        dispatch_async(dispatch_get_main_queue(), ^{
+            [self videoWriterDidFinish:videoWriter];
+        });
+    }];
+}
+
+
+// This method is triggered once the video writer is done and the video is ready (or there are errors...)
+-(void)videoWriterDidFinish:(AVAssetWriter*)videoWriter
+{
+    // Checking the status of the video wirter
+    if (videoWriter.status == AVAssetWriterStatusCompleted)
+    {
+        // Getting the output URL and validating we can save ir
+        NSURL *outputURL = videoWriter.outputURL;
+        
+        // Initialaing the video asset with the video embedded in this project
+        _videoAsset = [[AVURLAsset alloc]initWithURL:outputURL  options:nil];
+        
+        [self createTextVideo:self];
+    }
+    else
+    {
+        // Printing the error to the log
+        NSError *error = videoWriter.error;
+        NSLog(@"Error: %@",error.description);
+    }    
+}
+
+
+
+// Returning a new imaged scaled to the given size using a given image
+- (UIImage*) scaleImage: (UIImage*)originalImage toSize: (CGSize)newSize
+{
+    UIGraphicsBeginImageContext(newSize);
+    [originalImage drawInRect:CGRectMake(0, 0, newSize.width, newSize.height)];
+    UIImage *newImage = UIGraphicsGetImageFromCurrentImageContext();
+    UIGraphicsEndImageContext();
+    
+    return newImage;
+}
+
+// Creating a CVPixelBuffer from a CGImage
+- (CVPixelBufferRef) newPixelBufferFromCGImage: (CGImageRef) image
+{
+    CGSize frameSize = CGSizeMake(640.0, 480.0);
+    
+    NSDictionary *options = [NSDictionary dictionaryWithObjectsAndKeys:
+                             [NSNumber numberWithBool:YES], kCVPixelBufferCGImageCompatibilityKey,
+                             [NSNumber numberWithBool:YES], kCVPixelBufferCGBitmapContextCompatibilityKey,
+                             nil];
+    CVPixelBufferRef pxbuffer = NULL;
+    CVReturn status = CVPixelBufferCreate(kCFAllocatorDefault, frameSize.width,
+                                          frameSize.height, kCVPixelFormatType_32ARGB, (__bridge  CFDictionaryRef) options,
+                                          &pxbuffer);
+    NSParameterAssert(status == kCVReturnSuccess && pxbuffer != NULL);
+    
+    CVPixelBufferLockBaseAddress(pxbuffer, 0);
+    void *pxdata = CVPixelBufferGetBaseAddress(pxbuffer);
+    NSParameterAssert(pxdata != NULL);
+    
+    CGColorSpaceRef rgbColorSpace = CGColorSpaceCreateDeviceRGB();
+    CGContextRef context = CGBitmapContextCreate(pxdata, frameSize.width,
+                                                 frameSize.height, 8, 4*frameSize.width, rgbColorSpace,
+                                                 kCGImageAlphaNoneSkipFirst);
+    NSParameterAssert(context);
+    CGContextConcatCTM(context, CGAffineTransformIdentity);
+    CGContextDrawImage(context, CGRectMake(0, 0, CGImageGetWidth(image),
+                                           CGImageGetHeight(image)), image);
+    CGColorSpaceRelease(rgbColorSpace);
+    CGContextRelease(context);
+    
+    CVPixelBufferUnlockBaseAddress(pxbuffer, 0);
+    
+    return pxbuffer;
+}
+
+
 
 // This method is invoked when the user clicked on the return (done) button of a text field keyboard
 -(BOOL)textFieldShouldReturn:(UITextField *)textField
